f0713e6531de2a5c3c264eef04c866dcfdf083e1
Ovadykov Elvg <oelvg@mail.ru>
2024-12-05 16:19:31 +0300

    add some components

---
+++ components/shared/index.ts
@@ -0,0 +1,8 @@
+export { TopBar } from "./top-bar";
+export { SortPopup } from "./sort-popup";
+export { Categories } from "./categories";
+export { Title } from "./title";
+export { Header } from "./header";
+export { Container } from "./container";
+export { Filters } from "./filters";
+export { FilterCheckbox } from "./filter-checkbox";
